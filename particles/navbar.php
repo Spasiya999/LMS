d577efff77b16877819b1f10f418b93546f7615c
Pasindu <pasindug@procons.lk>
2024-03-06 13:07:26 +0530

    setup env

--- particles/navbar.php
+++ particles/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar bg-body-tertiary">
     <div class="container">
         <a class="navbar-brand">Navbar</a>
-        <a href="" class="me-2 btn btn-primary">Login / Register</a>
+        <a href="../login.php" class="me-2 btn btn-primary">Login / Register</a>
     </div>
 </nav>
