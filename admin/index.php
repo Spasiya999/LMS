3a36cb0bb9efcbdba2a7bde6c6e6ecc1dc9e8a62
Pasindu <pasindug@procons.lk>
2024-03-06 13:18:35 +0530

    admin env setup

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+    <?php include 'particles/css/css.php'; ?>
+</head>
+
+<body>
+    <?php include 'particles/navbar.php'; ?>
+    <!-- Main -->
+    <div class="container mt-3">
+        <h1>Admin</h1>
+    </div>
+    <!-- Main -->
+    <?php include 'particles/js/js.php'; ?>
+</body>
+
+</html>
